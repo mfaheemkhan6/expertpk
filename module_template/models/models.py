a957be0726e013acc4e29fcd2660a45566b4a5d7
Your Name <you@example.com>
2015-12-03 18:36:07 -0430

    rm module template

--- module_template/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class SaleMarginExtended(models.Model):
-#     _name = '../sale_margin_extended'
-
-#     name = fields.Char()
